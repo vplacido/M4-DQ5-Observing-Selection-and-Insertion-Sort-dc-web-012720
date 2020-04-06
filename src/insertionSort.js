5d9c7cf5c0ce5018c979303dd1ca47292275e776
vplacido <valentinplacido25@gmail.com>
2020-04-06 09:58:06 -0400

    lab finished

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,12 @@
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(let i =0; i<arr.length; i++) {
+    let value = arr[i];
+    let j = i-1;
+    while(j>=0 && arr[j]>value) {
+      arr[j+1] = arr[j];
+      j--;
+    }
+    arr[j+1] = value;
+  }
+  return arr;
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,16 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(let i=0; i< arr.length-1; i++) {
+    let min = i;
+    for(let j=i+1; j<arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
 }
